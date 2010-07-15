ea0b3422d9e2716b0631922a32bbb1e27f0abd3a
Brad Ediger <brad.ediger@madriska.com>
2010-07-15 10:17:51 -0500

    Clean up old code

--- lib/active_record/connection_adapters/mysql_replication_adapter/ar_base_ext.rb
+++ lib/active_record/connection_adapters/mysql_replication_adapter/ar_base_ext.rb
@@ -37,25 +37,6 @@ module ActiveRecord
         ConnectionAdapters::MysqlReplicationAdapter.new(mysql, logger, [host, username, password, database, port, socket], config)
       end
 
-      # make our standard checks.  First, ensure they asked for a slave.  Then, make sure our connection
-      # is in fact of the right type.  Finally, check to see if we're in a transaction.  If we are,
-      # use the master to ensure accuracy.
-      # Note that though the code is cleaner, this will increase call times by 2x over the 
-      # boolean check below.  
-      # From preliminary tests, for a simple query, I see about a 10% overhead to invoking the 
-      # load_balance_query, and another 5% overhead to invoking this run_on method with the yield.
-      def run_on_db(use_slave = nil)
-#        logger.debug("checking conn.  use_slave? #{use_slave} in trans? #{Thread.current['open_transactions']}") if logger && logger.debug
-        if (use_slave && 
-            connection.is_a?(ConnectionAdapters::MysqlReplicationAdapter) && 
-            (Thread.current['open_transactions'] || 0) == 0)
-
-          connection.load_balance_query { yield }
-        else
-          yield
-        end
-      end
-
       def slave_valid(use_slave = nil)
 #        logger.debug("checking conn.  use_slave? #{use_slave} in trans? #{Thread.current['open_transactions']}") if logger && logger.debug
         use_slave && 
@@ -73,9 +54,6 @@ module ActiveRecord
       # Override the standard find to check for the :use_slave option. When specified, the
       # resulting query will be sent to a slave machine.
       def find_every(options)
-#        run_on_db(options[:use_slave]) do
-#          old_find_every(options)
-#        end
         if slave_valid(options[:use_slave]) 
           connection.load_balance_query { old_find_every(options) }
         else
@@ -87,9 +65,6 @@ module ActiveRecord
       # Override find_by_sql so that you can tell it to selectively use a slave machine
       def find_by_sql(sql, use_slave = false)
         use_slave = get_use_slave(use_slave)
-#        run_on_db(use_slave) do
-#          old_find_by_sql sql
-#        end
         if slave_valid(use_slave)
           connection.load_balance_query { old_find_by_sql sql }
         else
@@ -100,9 +75,6 @@ module ActiveRecord
       alias_method :old_count_by_sql, :count_by_sql
       def count_by_sql(sql, use_slave = false)
         use_slave = get_use_slave(use_slave)
-#        run_on_db(use_slave) do
-#          old_count_by_sql sql
-#        end
         if slave_valid(use_slave)
           connection.load_balance_query { old_count_by_sql sql }
         else
@@ -114,9 +86,6 @@ module ActiveRecord
       alias_method :old_calculate, :calculate
       def calculate(operation, column_name, options ={})
         use_slave = options.delete(:use_slave)
-#        run_on_db(use_slave) do
-#          old_calculate(operation, column_name, options)
-#        end
         if slave_valid(use_slave)
           connection.load_balance_query { old_calculate(operation, column_name, options) }
         else
